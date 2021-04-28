cda2a4ff0bbb20508b3f1236326c14655247f876
Anvi Alex <epononanvialex@gmail.com>
2021-04-29 01:55:51 +0300

    Convert forms with form_tag then form_with and finally check errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -10,6 +11,21 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    p @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
 
