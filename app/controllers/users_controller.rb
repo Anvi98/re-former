51951fe0b79530df4f16a9db5028c8aa2ae724d1
Arthur Borges <arthurborges.dev@gmail.com>
2021-04-29 11:53:53 -0300

    Add microverse README and remove the debug code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,13 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    p @user
   end
 
   def update
     @user = User.find(params[:id])
 
     if @user.update(user_params)
-      redirect_to new_user_path
+      redirect_to edit_user_path
     else
       render :edit
     end
